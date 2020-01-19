31606d040711a5fd44f3c6370102db6701e7d34a
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2020-01-19 23:02:42 +0100

    Use key generation based on a password

--- examples/aead.rs
+++ examples/aead.rs
@@ -1,4 +1,6 @@
-use ring::aead::{Aad, BoundKey, Nonce, NONCE_LEN};
+use core::num::NonZeroU32;
+use ring::aead::{Aad, BoundKey, Nonce, MAX_TAG_LEN, NONCE_LEN};
+use ring::pbkdf2;
 use ring::rand::{SecureRandom, SystemRandom};
 use ring::{aead, error};
 
@@ -16,21 +18,10 @@ impl aead::NonceSequence for OneNonceSequence {
     }
 }
 
-fn generate_key<T: BoundKey<OneNonceSequence>>(rand_vec: [u8; 12]) -> T {
-    // generate unbound_key
-    let key = [0; 32];
-    let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &key).unwrap();
-
-    // generate nonce_sequence
-    let nonce = Nonce::assume_unique_for_key(rand_vec);
-    let nonce_sequence = OneNonceSequence::new(nonce);
-    T::new(unbound_key, nonce_sequence)
-}
-
 fn main() {
     let text = "content to encrypt";
     let mut in_out = text.as_bytes().to_vec();
-    println!("{:?}", &in_out);
+    println!("data {:?}", &in_out);
 
     // generate nonce
     let rand_gen = SystemRandom::new();
@@ -38,17 +29,42 @@ fn main() {
     rand_gen.fill(&mut rand_vec).unwrap();
 
     // encrypt
-    let mut s_key: aead::SealingKey<OneNonceSequence> = generate_key(rand_vec);
+    let mut key = [0; 32];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA256,
+        NonZeroU32::new(100).unwrap(),
+        &[0, 1, 2, 3, 4, 5, 6, 7],
+        b"nice password",
+        &mut key,
+    );
+    let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &key).unwrap();
+    let nonce = Nonce::assume_unique_for_key(rand_vec);
+    let nonce_sequence = OneNonceSequence::new(nonce);
+    let mut s_key: aead::SealingKey<OneNonceSequence> = BoundKey::new(unbound_key, nonce_sequence);
     s_key
         .seal_in_place_append_tag(Aad::empty(), &mut in_out)
         .unwrap();
-    println!("{:?}", &in_out);
+    println!("encrypted {:?}", &in_out);
 
     // decrypt
-    let mut o_key: aead::OpeningKey<OneNonceSequence> = generate_key(rand_vec);
-    o_key.open_in_place(Aad::empty(), &mut in_out).unwrap();
-    println!("{:?}", &in_out);
+    let mut key = [0; 32];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA256,
+        NonZeroU32::new(100).unwrap(),
+        &[0, 1, 2, 3, 4, 5, 6, 7],
+        b"nice password",
+        &mut key,
+    );
+    let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &key).unwrap();
+    // Q: HOW CAN I NOT USE RAND_VEC, SINCE IN REAL PROGRAM IT WON'T BE ACCESSIBLE, RIGHT?
+    let nonce = Nonce::assume_unique_for_key(rand_vec);
+    let nonce_sequence = OneNonceSequence::new(nonce);
+    let mut o_key: aead::OpeningKey<OneNonceSequence> = BoundKey::new(unbound_key, nonce_sequence);
+    o_key
+        .open_in_place(Aad::empty(), &mut in_out)
+        .expect("can't decrypt");
+    // Q: IS THIS CORRECT ANYWAY?
+    in_out.split_off(in_out.len() - MAX_TAG_LEN);
 
-    // how should i (in a real example) know the length of the original text?
     assert_eq!(in_out.as_slice(), text.as_bytes());
 }
